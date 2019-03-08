4bea0ff61f063e9daf2d1cbf1d72fc81eac2c431
Zemian Deng <zemiandeng@gmail.com>
2019-03-08 18:13:47 -0500

    Remove git add/ci

--- jmh-starter/run-test.sh
+++ jmh-starter/run-test.sh
@@ -3,5 +3,3 @@ TAGS=${1:-"$(uname -s)"}
 REPORT="target/${TAGS}_benchmarks-$(date +%Y%m%d%H%M%S).txt"
 echo "Saving report: $REPORT"
 java -jar target/benchmarks.jar -f 1 -wi 1 -i 1 -o "$REPORT"
-git add "$REPORT"
-git ci -m "Add $REPORT"
