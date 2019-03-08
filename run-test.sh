0f8acfe87cd6bafb7c5506da417d8631accd1bd6
Zemian Deng <zemiandeng@gmail.com>
2019-03-08 11:46:33 -0500

    Update run-test to use full date

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPORT="reports/test-$(uname -s)_$(date +%s).txt"
+REPORT="reports/test-$(uname -s)_$(date +%Y%m%d%H%M%S).txt"
 java -jar test/dist/benchmarks.jar | tee $REPORT
 git add "$REPORT"
 git ci -m "Add $REPORT"
