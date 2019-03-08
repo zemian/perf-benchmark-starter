1643d6a3a62d080f197ec3eef69ed61e628628d2
Zemian Deng <zemiandeng@gmail.com>
2019-03-08 15:09:07 -0500

    Rename reports

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
-REPORT="reports/benchmarks-$(uname -s)_$(date +%Y%m%d%H%M%S).txt"
-java -jar test/dist/benchmarks.jar -f 1 -wi 1 -i 1 -o $REPORT
+TAGS=${1:-""}
+REPORT="reports/test/benchmarks-$(uname -s)_${TAGS}$(date +%Y%m%d%H%M%S).txt"
+echo "Saving report: $REPORT"
+java -jar test/dist/benchmarks.jar -f 1 -wi 1 -i 1 -o "$REPORT"
 git add "$REPORT"
 git ci -m "Add $REPORT"
