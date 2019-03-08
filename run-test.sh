6c9a8b8dc13b9be1e213f0fbafecd223c875ff3c
Zemian Deng <zemiandeng@gmail.com>
2019-03-08 11:56:25 -0500

    Update run-test.sh to run short iterations.

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 REPORT="reports/test-$(uname -s)_$(date +%Y%m%d%H%M%S).txt"
-java -jar test/dist/benchmarks.jar | tee $REPORT
+java -jar test/dist/benchmarks.jar -f 1 -wi 1 -i 1 -o $REPORT
 git add "$REPORT"
 git ci -m "Add $REPORT"
